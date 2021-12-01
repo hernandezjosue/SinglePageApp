10e68c90b679bfe4847fbb2f21235e8fb881fb6a
Josue Hernandez <iseljosue@gmail.com>
2021-12-01 16:05:12 -0600

    my first commit

--- src/services/CountriesServices.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios')
-
- const URL = 'https://restcountries.com/v3.1/all';
- export const getCountries =()=>{
-   return  axios
-    .get(URL)
-   .then((result) => result) //setcountries
-   .catch((err) => console.log(err))
- }
